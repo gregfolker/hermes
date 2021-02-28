9d717e1f61b1b4c41561980fb56a7de794d46c03
gregfolker <greg.folker@pm.me>
2021-02-28 16:21:03 -0600

    feat: add new package to abstract file operations
    Currently only has static values for common filetype extensions

---
+++ pkg/files/files.go
@@ -0,0 +1,12 @@
+package files
+
+// Common file extensions
+const (
+	GO_EXT = ".go"
+	C_EXT = ".c"
+	PYTHON_EXT = ".py"
+	JAVA_EXT = ".java"
+	RUST_EXT = ".rs"
+	BASH_EXT = ".sh"
+	PERL_EXT = ".pl"
+)
