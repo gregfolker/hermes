a60ddfa0827a541dbf82c776f8d6f7e90c0b4420
gregfolker <greg.folker@pm.me>
2021-03-18 13:52:17 -0500

    fix: check for go installation before trying to build

--- install.sh
+++ install.sh
@@ -1,5 +1,11 @@
 BIN="_out/pbld"
 
+if command -v go > /dev/null 2>&1 ; then
+   echo "found $(go version)"
+else
+   echo "go not found. Please install golang using your systems package manager"
+fi
+
 if [[ -e ${BIN}  ]] ; then
-   sudo cp _out/pbld /usr/bin
+   cp _out/pbld /usr/local/bin
 fi
